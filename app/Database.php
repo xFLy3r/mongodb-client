d4855f1711b854b4421cc9d08a6f3a3f37dc992e
xFLy3r <datus3r@gmail.com>
2017-11-07 21:01:37 +0200

    Init project

---
+++ app/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This class responses for connection and actions with database
+ */
+class Database
+{
+    protected $client;
+
+    public function __construct()
+    {
+
+    }
+}
---
+++ app/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Database requirements
+ */
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'blog_mvc');
